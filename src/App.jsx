1d5b9ee322887384869e412347d4136dfd5dc22a
Hugo Vuillet <hugovuillet21@gmail.com>
2024-01-04 09:11:51 +0100

    projet start

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
